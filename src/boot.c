7de5ebe5c39d3767816db64d6b448fc8431c4bce
Matthew Leach <matthew@mattleach.net>
2018-01-28 15:38:12 +0000

    boot: init_clocking: new
    Add a function that brings the main CPU up to 100MHz operation.

--- src/boot.c
+++ src/boot.c
@@ -25,6 +25,7 @@
 #include <string.h>
 
 #include <lib/macros.h>
+#include <lpc17xx.h>
 
 #include "boot.h"
 #include "cpu.h"
@@ -41,6 +42,55 @@ void boot_main(void);
 uint8_t boot_stack[BOOT_STACK_SIZE] __aligned(CPU_STACK_ALIGN);
 
 static void
+init_clocking(void)
+{
+    /* Enable the main oscillator and set the correct frequency range
+     * (12Mhz). */
+    LPC_SC->SCS = (1 << 5);
+
+    /* Wait for Main oscillator startup. */
+    while (!(LPC_SC->SCS & (1 << 6))) {};
+
+    /* Set sysclk to the main oscillator. */
+    LPC_SC->CLKSRCSEL = 0x1;
+
+    /* Output from the PLL will be 300 Mhz.  We want to divide this
+     * down by 3 to produce the CCLK of 100 Mhz. */
+    LPC_SC->CCLKCFG = 0x2;
+
+    /* We want an output of 300Mhz from the PLL.  Assuming a
+     * pre-divide of 2, M = (300 * 2) / (2 * 12).  Therefore, set: M =
+     * 25 and N = 2. */
+    LPC_SC->PLL0CFG = 24 | (1 << 16);
+
+    /* Provide the PLL feed sequence. */
+    LPC_SC->PLL0FEED = 0xAA;
+    LPC_SC->PLL0FEED = 0x55;
+
+    /* Enable the PLL. */
+    LPC_SC->PLL0CON = 0x01;
+
+    /* Provide the PLL feed sequence. */
+    LPC_SC->PLL0FEED = 0xAA;
+    LPC_SC->PLL0FEED = 0x55;
+
+    /* Wait for PLL0 lock */
+    while (!(LPC_SC->PLL0STAT & (1 << 26))) {};
+
+    /* Enable and connect PLL0 */
+    LPC_SC->PLL0CON = 0x3;
+
+    /* Provide the PLL feed sequence. */
+    LPC_SC->PLL0FEED = 0xAA;
+    LPC_SC->PLL0FEED = 0x55;
+
+    /* Wait until PLL0 enable and connect. */
+    while (!(LPC_SC->PLL0STAT & ((1 << 25) | (1 << 24)))) {};
+
+    /* CPU should now be operating at 100 Mhz. */
+}
+
+static void
 boot_copy_data(void)
 {
     memcpy(&_data_start, &_lma_data_addr, &_data_end - &_data_start);
@@ -51,5 +101,6 @@ boot_main(void)
 {
     cpu_intr_disable();
     boot_copy_data();
+    init_clocking();
     main();
 }
