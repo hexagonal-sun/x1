36e4edb5be6a41991eecdbb94970451a8b9d8bc9
Matthew Leach <matthew@mattleach.net>
2018-04-17 22:28:24 +0100

    net: udp: implement buffered UDP
    Add a buffered UDP implementation, that allows a user to listen on a
    UDP port and RX data later.

--- src/net/udp.c
+++ src/net/udp.c
@@ -1,7 +1,9 @@
 #include <stdint.h>
 #include <errno.h>
 #include <string.h>
+#include <stdlib.h>
 #include <lib/byteswap.h>
+#include <lib/cbuf.h>
 #include <src/mutex.h>
 #include <src/condvar.h>
 
@@ -10,13 +12,14 @@
 #include "protocol.h"
 #include "udp.h"
 
+static const size_t RX_BUF_SZ = 256;
+
 typedef struct
 {
-    uint8_t *dst_buf;
-    size_t dst_buf_sz;
-    size_t dst_buf_ptr;
     uint16_t port;
     struct list node;
+    struct cbuf rx_buf;
+    void *rx_buf_buffer;
 } udp_listener;
 
 static LIST(udp_listeners);
@@ -26,36 +29,84 @@ static struct condvar udp_condvar;
 static void udp_swap_endian(udp_header *header)
 {
     swap_endian16(&header->dst_port);
+    swap_endian16(&header->src_port);
     swap_endian16(&header->length);
 }
 
-int udp_rx(uint16_t port, void *dst_buf, uint16_t dst_buf_sz)
+void *udp_listen(uint16_t port)
 {
     udp_listener *i;
-    udp_listener newListener;
+    udp_listener *newListener;
+    void *rx_buf;
 
     mutex_lock(&udp_mutex);
 
     list_for_each_entry(&udp_listeners, i, node)
         if (i->port == port)
-            return EINUSE;
+            return NULL;
 
-    newListener.port = port;
-    newListener.dst_buf = dst_buf;
-    newListener.dst_buf_sz = dst_buf_sz;
-    newListener.dst_buf_ptr = 0;
+    newListener = malloc(sizeof(*newListener));
 
-    list_insert_head(&udp_listeners, &newListener.node);
+    if (!newListener)
+        return NULL;
 
-    while (newListener.dst_buf_ptr !=
-           newListener.dst_buf_sz)
-        condvar_wait(&udp_condvar, &udp_mutex);
+    rx_buf = malloc(RX_BUF_SZ);
 
+    if (!rx_buf) {
+        free(newListener);
+        return NULL;
+    }
 
-    list_remove(&newListener.node);
+    newListener->port = port;
+    newListener->rx_buf_buffer = rx_buf;
+    cbuf_init(&newListener->rx_buf, rx_buf, RX_BUF_SZ);
+
+    list_insert_head(&udp_listeners, &newListener->node);
+
     mutex_unlock(&udp_mutex);
 
-    return newListener.dst_buf_ptr;
+    return (void *)newListener;
+}
+
+void udp_free(void *udp)
+{
+    udp_listener *listerner = (udp_listener *)udp;
+
+    mutex_lock(&udp_mutex);
+
+    list_remove(&listerner->node);
+
+    mutex_unlock(&udp_mutex);
+
+    free(listerner->rx_buf_buffer);
+    free(listerner);
+}
+
+int udp_rx_data(void *udp, void *dst_buf, uint16_t buf_len)
+{
+    udp_listener *listener = (udp_listener *)udp;
+    size_t bytes_copied = 0;
+    mutex_lock(&udp_mutex);
+
+    while (buf_len) {
+        size_t no_bytes_to_copy, bytes_in_buf;
+
+        while (cbuf_size(&listener->rx_buf) == 0)
+            condvar_wait(&udp_condvar, &udp_mutex);
+
+        bytes_in_buf = cbuf_size(&listener->rx_buf);
+        no_bytes_to_copy = bytes_in_buf > buf_len ? buf_len : bytes_in_buf;
+
+        cbuf_pop(&listener->rx_buf, dst_buf, &no_bytes_to_copy);
+
+        buf_len      -= no_bytes_to_copy;
+        dst_buf      += no_bytes_to_copy;
+        bytes_copied += no_bytes_to_copy;
+    }
+
+    mutex_unlock(&udp_mutex);
+
+    return bytes_copied;
 }
 
 static void udp_rx_packet(struct packet_t *pkt)
@@ -80,16 +131,8 @@ static void udp_rx_packet(struct packet_t *pkt)
     mutex_lock(&udp_mutex);
     list_for_each_entry(&udp_listeners, i, node) {
         if (i->port == header->dst_port) {
-            size_t user_buf_sz = i->dst_buf_sz - i->dst_buf_ptr,
-                   no_bytes_to_copy = user_buf_sz;
+            cbuf_push(&i->rx_buf, udp_payload, udp_payload_sz, 0);
 
-
-            if (udp_payload_sz < no_bytes_to_copy)
-                no_bytes_to_copy = udp_payload_sz;
-
-            memcpy(i->dst_buf + i->dst_buf_ptr, udp_payload, no_bytes_to_copy);
-
-            i->dst_buf_ptr += no_bytes_to_copy;
             condvar_signal(&udp_condvar);
             break;
         }
@@ -97,14 +140,14 @@ static void udp_rx_packet(struct packet_t *pkt)
     mutex_unlock(&udp_mutex);
 }
 
-void udp_xmit_packet_paylaod(uint16_t dst_port, uint32_t dst_ip,
-                     struct packet_t *payload)
+void udp_xmit_packet_paylaod(uint16_t src_port, uint16_t dst_port,
+                             uint32_t dst_ip, struct packet_t *payload)
 {
     udp_header header;
 
     memset(&header, 0, sizeof(header));
 
-    header.src_port = 0;
+    header.src_port = src_port;
     header.dst_port = dst_port;
     header.length = sizeof(header) + payload->tx.total_len;
     header.checksum = 0;
@@ -119,8 +162,8 @@ void udp_xmit_packet_paylaod(uint16_t dst_port, uint32_t dst_ip,
     protocol_inject_tx(payload, IPV4);
 }
 
-void udp_xmit_packet(uint16_t dst_port, uint32_t dst_ip, void *payload,
-                     int payload_len)
+void udp_xmit_packet(uint16_t src_port, uint16_t dst_port, uint32_t dst_ip,
+                     void *payload, int payload_len)
 {
     struct packet_t *pkt = packet_tx_create();
 
@@ -129,7 +172,7 @@ void udp_xmit_packet(uint16_t dst_port, uint32_t dst_ip, void *payload,
 
     packet_tx_push_header(pkt, payload, payload_len);
 
-    udp_xmit_packet_paylaod(dst_port, dst_ip, pkt);
+    udp_xmit_packet_paylaod(src_port, dst_port, dst_ip, pkt);
 }
 
 static struct protocol_t udp_procotol = {
--- src/net/udp.h
+++ src/net/udp.h
@@ -10,12 +10,15 @@ typedef struct {
     uint16_t checksum;
 } udp_header;
 
-int udp_rx(uint16_t port, void *dst_buf, uint16_t dst_buf_sz);
+void *udp_listen(uint16_t port);
+void udp_free(void *udp);
 
-void udp_xmit_packet(uint16_t dst_port, uint32_t dst_ip, void *payload,
-                     int payload_len);
+int udp_rx_data(void *udp, void *dst_buf, uint16_t buf_len);
 
-void udp_xmit_packet_paylaod(uint16_t dst_port, uint32_t dst_ip,
-                             struct packet_t *payload);
+void udp_xmit_packet(uint16_t src_port, uint16_t dst_port, uint32_t dst_ip,
+                     void *payload, int payload_len);
+
+void udp_xmit_packet_paylaod(uint16_t src_port, uint16_t dst_port,
+                             uint32_t dst_ip, struct packet_t *payload);
 
 void udp_init(void);
