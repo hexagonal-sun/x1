d871a20215cbd89829b8439d68f6ffff4f228e4a
Matthew Leach <matthew@mattleach.net>
2018-07-15 20:47:25 +0100

    include/network/dns: document the api
    Add some comments that explain how to use the API.

--- include/network/dns.h
+++ include/network/dns.h
@@ -1,5 +1,11 @@
 #pragma once
 #include <stdint.h>
 
+/* Return a textual string that describes any errors returned by
+ * `dns_resolve_ipv4'. */
 const char *dns_get_error(int ret);
+
+/* Attempt to resolve a given `hostname' into an `ipv4_address'.
+ *
+ * Returns 0 on success. */
 int dns_resolve_ipv4(const char *hostname, uint32_t *ipv4_address);
